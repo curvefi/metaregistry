25a745cb21302dad73531cddeab128afc2d6b561
Edward Amor <edward.amor3@gmail.com>
2022-06-07 21:29:58 +0000

    chore: add script for identifying missing view methods
    Since the metaregistry is a wrapper around all 4 exisiting registries, imperative that a end user should be able to have the abi of one of the registries and still make queries the same way

---
+++ scripts/print_missing.py
@@ -0,0 +1,14 @@
+from brownie import MetaRegistry, interface
+
+
+def main():
+    meta_selectors = set(MetaRegistry.selectors)
+
+    for registry_name in [f"{a}{b}" for a in ["Crypto", "Stable"] for b in ["Factory", "Registry"]]:
+        registry = getattr(interface, registry_name)
+        print(registry_name, "*" * len(registry_name), sep="\n")
+
+        view_fns = {d["name"] for d in registry.abi if d.get("stateMutability") == "view"}
+        missing_fns = {registry.selectors[k] for k in (set(registry.selectors) - meta_selectors)}
+
+        print("\n".join(sorted(missing_fns & view_fns)), "\n")
