39c03160852a29ff017eeb6cf18ef45a8ef489c4
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-12-15 10:10:25 +0530

    sbtc2 integration

--- scripts/change_registry_handler.py
+++ scripts/change_registry_handler.py
@@ -1,15 +1,15 @@
 import sys
+
 from brownie import (
-    accounts,
-    network,
-    MetaRegistry,
-    ProxyAdmin,
     AddressProvider,
     CryptoRegistry,
+    MetaRegistry,
+    ProxyAdmin,
+    accounts,
+    network,
 )
 from brownie.network.gas.strategies import GasNowScalingStrategy
 from rich.console import Console as RichConsole
-
 
 RICH_CONSOLE = RichConsole(file=sys.stdout)
 ADDRESS_PROVIDER = "0x0000000022D53366457F9d5E68Ec105046FC4383"
@@ -31,32 +31,49 @@ if network.show_active() == "mainnet":
 
 else:
     RICH_CONSOLE.log("Simulation Mode. Writing to mainnet-fork.")
-    txparams = {"from": accounts.at("0xbabe61887f1de2713c6f97e567623453d3C79f67", force=True)}
-    
+    txparams = {
+        "from": accounts.at(
+            "0xbabe61887f1de2713c6f97e567623453d3C79f67", force=True
+        )
+    }
+
 
 def main():
-    
+
     # admin only: only admin of ADDRESSPROVIDER's proxy admin can do the following:
     address_provider = AddressProvider.at(ADDRESS_PROVIDER)
     address_provider_admin = address_provider.admin()
     proxy_admin = ProxyAdmin.at(address_provider_admin)
-    
-    metaregistry = MetaRegistry.at("0xF98B45FA17DE75FB1aD0e7aFD971b0ca00e379fC")
-    
-    assert metaregistry.get_registry(CRYPTO_REGISTRY_HANDLER_ID) == OLD_CRYPTO_REGISTRY_HANDLER
+
+    metaregistry = MetaRegistry.at(
+        "0xF98B45FA17DE75FB1aD0e7aFD971b0ca00e379fC"
+    )
+
+    assert (
+        metaregistry.get_registry(CRYPTO_REGISTRY_HANDLER_ID)
+        == OLD_CRYPTO_REGISTRY_HANDLER
+    )
     num_pools = metaregistry.pool_count()
     num_registries = metaregistry.registry_length()
-    num_pools_in_crypto_registry = CryptoRegistry.at(CRYPTO_REGISTRY).pool_count()
-    
+    num_pools_in_crypto_registry = CryptoRegistry.at(
+        CRYPTO_REGISTRY
+    ).pool_count()
+
     call_data = metaregistry.update_registry_handler.encode_input(
-        CRYPTO_REGISTRY_HANDLER_ID, CRYPTO_REGISTRY_HANDLER,
+        CRYPTO_REGISTRY_HANDLER_ID,
+        CRYPTO_REGISTRY_HANDLER,
     )
-    tx = proxy_admin.execute(metaregistry.address, call_data, txparams)
-    
-    assert metaregistry.pool_count() == num_pools + num_pools_in_crypto_registry
+    proxy_admin.execute(metaregistry.address, call_data, txparams)
+
+    assert (
+        metaregistry.pool_count() == num_pools + num_pools_in_crypto_registry
+    )
     assert metaregistry.registry_length() == num_registries
-    assert metaregistry.get_gauge("0xD51a44d3FaE010294C616388b506AcdA1bfAAE46") != ZERO_ADDRESS
-    
+    assert (
+        metaregistry.get_gauge("0xD51a44d3FaE010294C616388b506AcdA1bfAAE46")
+        != ZERO_ADDRESS
+    )
+
 
 if __name__ == "__main__":
     main()
--- scripts/deploy_crypto_registry_handler.py
+++ scripts/deploy_crypto_registry_handler.py
@@ -3,8 +3,8 @@ from ape import project
 from ape.cli import NetworkBoundCommand, account_option, network_option
 from eth_abi import encode
 
-
 CRYPTO_REGISTRY_ADDRESS = "0x9a32aF1A11D9c937aEa61A3790C2983257eA8Bc0"
+
 
 @click.group(short_help="Deploy the registry handler")
 def cli():
@@ -15,7 +15,7 @@ def cli():
 @network_option()
 @account_option()
 def main(network, account):
-    
+
     print(
         "Crypto Registry Handler constructor arguments: ",
         encode(["address"], [CRYPTO_REGISTRY_ADDRESS]).hex(),
