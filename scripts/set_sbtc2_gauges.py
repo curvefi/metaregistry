e5d2b8290b62921473d1ea8cbeeecd6fd4084029
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-12-21 18:51:39 +0100

    working script (in mainnet)

--- scripts/set_sbtc2_gauges.py
+++ scripts/set_sbtc2_gauges.py
@@ -2,34 +2,16 @@ from brownie import (
     Contract,
     ZERO_ADDRESS,
     accounts,
-    network,
+    config
 )
 
-from brownie.network.gas.strategies import GasNowScalingStrategy
+config['autofetch_sources'] = True
 
 ADDRESS_PROVIDER = "0x0000000022D53366457F9d5E68Ec105046FC4383"
+babe = accounts.load('babe')
 txparams = {
-    "from": accounts.at(
-        "0xbabe61887f1de2713c6f97e567623453d3C79f67", force=True
-    )
+    "from": babe, 'priority_fee': '1 gwei', 'required_confs': 5
 }
-
-def init_contract(contract_addr: str):
-    try:
-        return Contract(contract_addr)
-    except:
-        return Contract.from_explorer(contract_addr)
-
-
-if network.show_active() == "mainnet":
-    print("Deploying on mainnet")
-    accounts.load("babe")
-    txparams = {"from": accounts[0], "required_confs": 5}
-    try:
-        network.gas_price(GasNowScalingStrategy("slow", "fast"))
-    except ConnectionError:
-        pass
-    
 
 POOL_DATA = {
     "pool": "0xf253f83AcA21aAbD2A20553AE0BF7F65C755A07F",
@@ -40,20 +22,20 @@ POOL_DATA = {
     "is_v2": True,
     "gauge": "0x6D787113F23bED1D5e1530402B3f364D0A6e5Af3"
 }
-    
+
 
 def main():
 
     # admin only: only admin of ADDRESSPROVIDER's proxy admin can do the following:
-    address_provider = init_contract(ADDRESS_PROVIDER)
+    address_provider = Contract(ADDRESS_PROVIDER)
     address_provider_admin = address_provider.admin()
-    proxy_admin = init_contract(address_provider_admin)
-    base_pool_registry = init_contract("0xDE3eAD9B2145bBA2EB74007e58ED07308716B725")
-    stable_registry = init_contract("0x90E00ACe148ca3b23Ac1bC8C240C2a7Dd9c2d7f5")
-    metaregistry = init_contract("0xF98B45FA17DE75FB1aD0e7aFD971b0ca00e379fC")
-    
+    proxy_admin = Contract(address_provider_admin)
+    base_pool_registry = Contract("0xDE3eAD9B2145bBA2EB74007e58ED07308716B725")
+    stable_registry = Contract("0x90E00ACe148ca3b23Ac1bC8C240C2a7Dd9c2d7f5")
+    metaregistry = Contract("0xF98B45FA17DE75FB1aD0e7aFD971b0ca00e379fC")
+
     assert not metaregistry.get_gauge(POOL_DATA["pool"]) == POOL_DATA["gauge"]
-    
+
     # --- add base pool to base pool registry ---
     call_data = base_pool_registry.add_base_pool.encode_input(
             POOL_DATA["pool"],
@@ -70,19 +52,19 @@ def main():
             base_pool_registry.base_pool_list(base_pool_index)
             == POOL_DATA["pool"]
         )
-    
+
     # --- add base pool's gauge to base registry ---
     gauges = stable_registry.get_gauges(POOL_DATA["pool"])
     assert gauges[0][0] == ZERO_ADDRESS
-    
+
     call_data = stable_registry.set_liquidity_gauges.encode_input(
         POOL_DATA["pool"],
         [POOL_DATA["gauge"]] + [ZERO_ADDRESS]*9
     )
     proxy_admin.execute(stable_registry, call_data, txparams)
-    
+
     gauges = stable_registry.get_gauges(POOL_DATA["pool"])
-    assert gauges[0][0] == POOL_DATA["gauge"] 
+    assert gauges[0][0] == POOL_DATA["gauge"]
 
     # check if metaregistry has the gauge
     assert metaregistry.get_gauge(POOL_DATA["pool"]) == POOL_DATA["gauge"]
